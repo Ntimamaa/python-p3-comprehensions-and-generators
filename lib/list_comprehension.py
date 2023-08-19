cda8790bb9c19240562c185bb6e4d9323d68f45f
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-08-19 09:57:01 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    evens = [num for num in sequence if num % 2 == 0]
+    return evens
 
-def make_exclamation(sentence_list):
-    pass
+result = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(result)  # Output: [0, 8]
+
+def make_exclamation(sentences):
+    exclamations = [sentence + "!" for sentence in sentences]
+    return exclamations
+
+result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(result)  # Output: ["Hello!", "I'm doing great!", "Python is fun!"]
